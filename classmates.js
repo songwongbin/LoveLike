5e3a230bd1b085d4fb011fa05386935e7655b590
songwongbin <bb201802@naver.com>
2024-11-13 22:04:43 +0900

    [update&fix]
    [update]
    1. talk 메서드 개편
    - 플레이어의 말솜씨 스텟에 비례해 증가폭은 늘리고 감소폭은 줄임
    - num 변수를 추가해 상대방 특성에 따라 증가 감소 결과가 달라지도록 함
    
    [fix]
    1. Classmate 클래스의 매개변수로 me를 통째로 받음
    - 플레이어의 스텟에 따라 상호작용 결과를 달리함에 있어서, 스텟 변화를 실시간으로 적용받기 위함
    2. myRoomScene 함수의 불필요한 매개변수를 제거
    - 내 방에선 classmate가 끼어들 요소가 없기 때문에 제거!

--- classmates.js
+++ classmates.js
@@ -2,29 +2,58 @@ import chalk from "chalk";
 import readlineSync from "readline-sync";
 
 export class Classmate {
-  constructor(stage) {
+  constructor(stage, player) {
     this.name = ["김지우", "박유빈", "송가을", "이세원", "이규민"];
     this.stage = stage; // 현재 스테이지 정보
     this.closeness = 60 - stage * 10; // 기본 친밀도 (스테이지 오를수록 낮아짐)
     this.isDate = false; // 연인 여부
     this.isIncrease = true; // 상호작용에 따른 친밀도 증감 여부
     this.isFailConfess = false; // 고백 실패시 일정확률로 게임오버 만들기 위한 체크
+    this.player = player; // 플레이어 인스턴스를 그대로 가져옴!!
   }
   talk() {
     this.isIncrease = true; // 친밀도 증감 여부 초기화
+    let num = 0; // 친밀도 증감 변수 초기화
     console.log(chalk.green(`대화 주제를 선택하세요!`));
     console.log(chalk.green(`1. 나 2. ${this.name[this.stage - 1]} 3. 게임`));
     const topics = readlineSync.question("입력 : ");
     switch (topics) {
       case "1":
-        this.closeness += 5;
+        // 화제로 나 선택
+        if (this.stage === 1 || this.stage === 3) {
+          num = 7; // 지우, 가을은 나한테 관심 있어서 크게 증가
+        } else if (this.stage === 4 || this.stage === 5) {
+          num = -5; // 세원, 규민은 나한테 관심 없어서 감소
+          this.isIncrease = false;
+        } else {
+          num = 5; // 유빈은 아무 생각 없어서 기본 증가
+        }
+        // 캐릭별 특징과 플레이어의 스텟을 반영한 친밀도 증감
+        this.closeness += Math.round(num * (1 + this.player.talkSkills / 100));
         break;
       case "2":
-        this.closeness += 10;
+        // 화제로 상대방 선택
+        if (this.stage === 5) {
+          num = 7; // 규민은 관심 가져주는 게 고마워서 크게 증가
+        } else if (this.stage === 3 || this.stage === 4) {
+          num = -5; // 가을은 츤데레라 관심 가지면 우쭐해져서, 세원은 귀찮아해서 감소
+          this.isIncrease = false;
+        } else {
+          num = 5; // 지유는 뭔들 좋아서, 유빈은 아무 생각 없어서 기본 증가
+        }
+        // 캐릭별 특징과 플레이어의 스텟을 반영한 친밀도 증감
+        this.closeness += Math.round(num * (1 + this.player.talkSkills / 100));
         break;
       case "3":
-        this.closeness -= 5;
-        this.isIncrease = false;
+        // 화제로 게임 선택
+        if (this.stage === 4) {
+          num = 7; // 세원은 은밀하게 게임을 좋아해서 크게 증가
+        } else {
+          num = -5; // 나머지는 게임에 별 관심 없어서 감소
+          this.isIncrease = false;
+        }
+        // 캐릭별 특징과 플레이어의 스텟을 반영한 친밀도 증감
+        this.closeness += Math.round(num * (1 - this.player.talkSkills / 100));
         break;
       default:
         console.log(chalk.red("1,2,3만 입력 가능합니다"));
